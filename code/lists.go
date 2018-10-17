3badea816b20d0e2b81f64eae71492cdaf408849
grendach <dmytro.grendach@nokia.com>
2018-10-17 23:42:13 +0200

    add slices

---
+++ code/lists.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [3]int //initiate list [0.0.0]
+	arr[0] = 4 //assign value to element
+	colors := []string{"blue", "black", "red"} //SLICES - when you don't know a size of a list
+	colors = append(colors, "orange") //append == recreation
+	// length, capacity
+	fmt.Println(len(colors))
+	fmt.Println(cap(colors))
+	// show results
+	fmt.Println("slice 'colors': ", colors)
+	fmt.Println("list 'arr': ", arr)
+}
