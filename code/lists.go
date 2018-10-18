8e4d420dee7904bda7a9d2cfcf4fb5576dc1850d
grendach <dmytro.grendach@nokia.com>
2018-10-18 15:09:06 +0200

    code refactoring

--- code/lists.go
+++ code/lists.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var arr [3]int //initiate list [0.0.0]
-	arr[0] = 4 //assign value to element
+	var arr [3]int                             //initiate list [0.0.0]
+	arr[0] = 4                                 //assign value to element
 	colors := []string{"blue", "black", "red"} //SLICES - when you don't know a size of a list
-	colors = append(colors, "orange") //append == recreation
+	colors = append(colors, "orange")          //append == recreation
+
 	// length, capacity
 	fmt.Println(len(colors))
 	fmt.Println(cap(colors))
+
 	// show results
 	fmt.Println("slice 'colors': ", colors)
 	fmt.Println("list 'arr': ", arr)
---
+++ code/loop.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//only for loops available in Golang
+func main() {
+	//loop from 0 until 10 is reached
+	for i := 1; i <= 10; i++ {
+		fmt.Println("go=", i)
+	}
+
+	list := []int{1, 3, 45, 13, 59, 0}
+	//loop itarates for a range over a slice or map.
+	for z, v := range list {
+		fmt.Printf("Element %d = %d\n", z, v)
+	}
+}
+
